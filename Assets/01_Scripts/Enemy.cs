2abbdab309ec68d024db74acec3eba0ca288a77e
Christian7547 <eliasgonzalesec@gmail.com>
2024-10-12 13:49:27 -0400

    Enemy attack & invocations

--- Assets/01_Scripts/Enemy.cs
+++ Assets/01_Scripts/Enemy.cs
@@ -1,16 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum TypeEnemy
+{
+    Wizard, Flying, Rider, Warrior
+}
 
 public class Enemy : MonoBehaviour
 {
     [Header("Stats")]
+    public TypeEnemy type;
     public float speed = 5f;
 
     [Header("References")]
-    public Rigidbody rb;
     public GameObject children;
+    public GameObject invocationPrefab;
+    public Transform firePoint;
 
+    bool canAttack = true;
+    float timer = 0f;
+    float timeBtwInvocation = 3f;
     Animator childrenAnimator;
     Transform target;
 
@@ -21,27 +29,107 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void Update()
     {
-        Movement();
+        CheckTypeEnemy();
     }
 
-    void Movement()
+    void CheckTypeEnemy()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        if (Vector3.Distance(transform.position, target.position) < 10)
+        if (target != null)
         {
-            if (transform.position.x < target.position.x)
-                children.transform.rotation = Quaternion.Euler(-60, -180, 0);
-            else
-                children.transform.rotation = Quaternion.Euler(60, 0, 0);
-            childrenAnimator.SetBool("isMoving", true);
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            switch (type)
+            {
+                case TypeEnemy.Wizard:
+                    if (Vector3.Distance(transform.position, target.position) > 10)
+                    {
+                        MoveTowardsTarget();
+                    }
+                    else
+                    {
+                        childrenAnimator.SetBool("isMoving", false);
+                        CheckIfCanAttack();
+                        CreateInvocation();
+                    }
+                    break;
+                case TypeEnemy.Rider:
+                    if (Vector3.Distance(transform.position, target.position) > 0.05f)
+                    {
+                        MoveTowardsTarget();
+                    }
+                    else
+                    {
+                        childrenAnimator.SetBool("isMoving", false);
+                        CheckIfCanAttack();
+                        Melee();
+                    }
+                    break;
+                case TypeEnemy.Warrior:
+                    if (Vector3.Distance(transform.position, target.position) > 2f)
+                    {
+                        MoveTowardsTarget();
+                    }
+                    else
+                    {
+                        childrenAnimator.SetBool("isMoving", false);
+                        CheckIfCanAttack();
+                        Melee();
+                    }
+                    speed = 3f;
+                    break;
+            }
+            Rotation();
         }
+    }
+
+    void MoveTowardsTarget()
+    {
+        childrenAnimator.SetBool("isMoving", true);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    void Rotation()
+    {
+        if (transform.position.x < target.position.x)
+            children.transform.rotation = Quaternion.Euler(-60, -180, 0);
         else
-            childrenAnimator.SetBool("isMoving", false);
+            children.transform.rotation = Quaternion.Euler(60, 0, 0);
+    }
+
+    void CheckIfCanAttack()
+    {
+        if (!canAttack)
+        {
+            childrenAnimator.ResetTrigger("attack");
+            timer += Time.deltaTime;
+            if (timer >= timeBtwInvocation)
+            {
+                timer = 0f;
+                canAttack = true;
+            }
+        }
+
+    }
+
+    void CreateInvocation()
+    {
+        if (canAttack)
+        {
+            childrenAnimator.SetTrigger("attack");
+            Instantiate(invocationPrefab, firePoint.position, Quaternion.identity);
+            canAttack = false;
+        }
+    }
+
+    void Melee()
+    {
+        if (canAttack)
+        {
+            childrenAnimator.SetTrigger("attack");
+            canAttack = false;
+        }
     }
 }
---
+++ Assets/01_Scripts/Invocation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Invocation : MonoBehaviour
+{
+    [Header("Stats")]
+    public int hp = 2;
+
+    [Header("References")]
+    public GameObject invocationPrefab;
+    float speed = 3f;
+
+    Transform target;
+    Animator childrenAnimator;
+    public GameObject children;
+
+    void Awake()
+    {
+        childrenAnimator = children.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Destroyer();
+    }
+
+    void Movement()
+    {
+        if (target != null)
+        {
+            if (transform.position.x < target.position.x)
+                children.transform.rotation = Quaternion.Euler(-60, -180, 0);
+            else
+                children.transform.rotation = Quaternion.Euler(60, 0, 0);
+            childrenAnimator.SetBool("isMoving", true);
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    void Destroyer() => Destroy(gameObject);
+}
