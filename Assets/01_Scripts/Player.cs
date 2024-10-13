e19439308e31185c72baee93a434078f092802fc
teddyFernandez7 <fernandezteddy04@gmail.com>
2024-10-13 01:22:53 -0400

    Finished store

--- Assets/01_Scripts/Player.cs
+++ Assets/01_Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 {
     [Header("Stats")]
     public float speed = 7f;
+    private float defaultSpeed;
 
     [Header("References")]
     public GameObject children;
@@ -26,6 +27,10 @@ public class Player : MonoBehaviour
     private float costOfIagra = 20f;
     private float costOfClona = 30f;
 
+    public GameObject clonaDialoguePanel; 
+    public TMP_Text clonaDialogueText;
+    private bool isClonaDialogueActive = false;
+
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -39,12 +44,19 @@ public class Player : MonoBehaviour
         inventory.iagra = 0f;
         inventory.clona = 0f;
         healthIndicator.text = lifes.ToString();
+        defaultSpeed = speed;
+        clonaDialoguePanel.SetActive(false);
     }
 
     void Update()
     {
         Movement();
         Attack();
+
+        if (isClonaDialogueActive && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CloseClonaDialogue();
+        }
     }
 
     void Movement()
@@ -57,7 +69,7 @@ public class Player : MonoBehaviour
 
     void Rotation(float toRotate)
     {
-        if(toRotate > 0)
+        if (toRotate > 0)
         {
             weapon.toRight = false;
             children.transform.rotation = Quaternion.Euler(-60, 180, 0);
@@ -87,7 +99,7 @@ public class Player : MonoBehaviour
 
     public void Healing()
     {
-        if(lifes < 4)
+        if (lifes < 4)
         {
             lifes += 1;
             PrintCurrentLifes();
@@ -104,12 +116,10 @@ public class Player : MonoBehaviour
     {
         if (inventory.Coin >= costOfTuki)
         {
-
             inventory.Coin -= costOfTuki;
             coinsLabel.text = inventory.Coin.ToString();
             inventory.AddTuki();
             PrintItemQuantityTuki();
-
         }
         else
         {
@@ -121,7 +131,6 @@ public class Player : MonoBehaviour
     {
         if (inventory.Coin >= costOfIagra)
         {
-
             inventory.Coin -= costOfIagra;
             coinsLabel.text = inventory.Coin.ToString();
             inventory.AddIagra();
@@ -137,7 +146,6 @@ public class Player : MonoBehaviour
     {
         if (inventory.Coin >= costOfClona)
         {
-
             inventory.Coin -= costOfClona;
             coinsLabel.text = inventory.Coin.ToString();
             inventory.AddClona();
@@ -164,6 +172,68 @@ public class Player : MonoBehaviour
     public void PrintItemQuantityClona()
     {
         itemClonaLabel.text = inventory.clona.ToString();
+    }
+    #endregion
+
+    #region Tuki
+    public void UseTuki()
+    {
+        if (inventory.tuki > 0)
+        {
+            Healing();  
+            inventory.tuki--; 
+            PrintItemQuantityTuki(); 
+        }
+        else
+        {
+            Debug.Log("No tienes esta habilidad.");
+        }
+    }
+    #endregion
+
+    #region Iagra
+    public void UseIagra()
+    {
+        if (inventory.iagra > 0)
+        {
+            speed += 5f; 
+            inventory.iagra--; 
+            PrintItemQuantityIagra(); 
+            Debug.Log("Velocidad aumentada.");
+        }
+        else
+        {
+            Debug.Log("No tienes esta habilidad.");
+        }
+    }
+    #endregion
+
+    #region Clona
+    public void UseClona()
+    {
+        if (inventory.clona > 0)
+        {
+            ShowClonaDialogue();
+            inventory.clona--;
+            PrintItemQuantityClona();
+        }
+        else
+        {
+            Debug.Log("No tienes esta habilidad.");
+        }
+    }
+
+    private void ShowClonaDialogue()
+    {
+        clonaDialoguePanel.SetActive(true);
+        clonaDialogueText.text = "No hace nada, solo cura tu depresión.";
+        isClonaDialogueActive = true;
+    }
+
+    private void CloseClonaDialogue()
+    {
+        clonaDialoguePanel.SetActive(false); 
+        isClonaDialogueActive = false;
     }
     #endregion
 }
